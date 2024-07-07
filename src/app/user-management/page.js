a133c8071fc7fd81fc9d09ba513bcd9c1a35f148
ashrafabusunaina1986 <ashrafabusunaina1986@gmail.com>
2024-07-07 21:34:04 +0300

    first commit

--- src/app/user-management/page.js
+++ src/app/user-management/page.js
@@ -7,12 +7,12 @@ import React from "react";
 export default async function UserManagement() {
   const users = (await fetchDataUser()).getUsers;
   return (
-    <div className="p-20">
+    <div className="p-10">
       <div className="flex justify-between">
         <h1>user managment</h1>
         <AddUser />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 sm:font-semibold sm:text-sm lg:grid-cols-3 gap-3 mt-10">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 sm:font-medium sm:text-sm lg:grid-cols-3 gap-3 mt-10">
         {users.map((user) => (
           <SingleUser user={user} />
         ))}
