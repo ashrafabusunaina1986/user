d8cf4bd854195076231d5b6bf70f0fe6b63a22cc
ashrafabusunaina1986 <ashrafabusunaina1986@gmail.com>
2024-07-08 14:49:00 +0300

    first commit

--- src/app/user-management/page.js
+++ src/app/user-management/page.js
@@ -13,9 +13,9 @@ export default async function UserManagement() {
         <AddUser />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 sm:font-medium sm:text-sm lg:grid-cols-3 gap-3 mt-10">
-        {users.map((user) => (
-          <SingleUser user={user} />
-        ))}
+        {users &&
+          users.length > 0 &&
+          users.map((user) => <SingleUser user={user} />)}
       </div>
     </div>
   );
